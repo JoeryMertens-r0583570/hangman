7d5d50af5664a83b0aa0e59dbc13fe52f70e8681
Wiietse Vandeput <wietse.vandeput@hotmail.com>
2018-04-16 15:07:29 +0200

    niks

--- Hangman/src/ui/Launcher.java
+++ Hangman/src/ui/Launcher.java
@@ -3,11 +3,14 @@ package ui;
 import javax.swing.*;
 
 public class Launcher {
-	
+
 	public static void main(String[] args) {
-		
+
 		JFrame f = new JFrame();
 		JOptionPane.showInputDialog(f, "Welkom! \nHoe heet je?");
 		JOptionPane.showMessageDialog(f, "... heeft als score: 1");
+		Object[] shapes = { "Cirkel", "Rechthoek" };
+		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE,
+				null, shapes, null);
 	}
 }
