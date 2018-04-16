38676e09e40ff41a5f735ec89b983593aa178250
Wiietse Vandeput <wietse.vandeput@hotmail.com>
2018-04-16 14:39:19 +0200

    rechthoek implementation

--- Hangman/src/domain/Rechthoek.java
+++ Hangman/src/domain/Rechthoek.java
@@ -1,5 +1,45 @@
 package domain;
 
 public class Rechthoek {
+	
+	private int breedte;
+	private int hoogte;
+	private Punt linkerBovenhoek;
+	
+	public Rechthoek(Punt LinkerBovenhoek, int breedte, int hoogte) {
+		
+	}
+
+	public int getBreedte() {
+		return breedte;
+	}
+
+	private void setBreedte(int breedte) {
+		this.breedte = breedte;
+	}
+
+	public int getHoogte() {
+		return hoogte;
+	}
+
+	private void setHoogte(int hoogte) {
+		this.hoogte = hoogte;
+	}
+
+	public Punt getLinkerBovenhoek() {
+		return linkerBovenhoek;
+	}
+
+	private void setLinkerBovenhoek(Punt linkerBovenhoek) {
+		this.linkerBovenhoek = linkerBovenhoek;
+	}
+	
+	public boolean equals(Rechthoek object) {
+		return false;
+	}
+	
+	public String toString() {
+		return ("Rechthoek: "+linkerBovenhoek.toString()+" - breedte: "+breedte+" - hoogte: "+hoogte);
+	}
 
 }
