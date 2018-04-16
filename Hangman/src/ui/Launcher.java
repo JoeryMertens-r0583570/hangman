96300f26a3b71f2dcf028b90903a49406a6a6776
Unknown <joery.mertens@student.ucll.be>
2018-04-16 15:16:54 +0200

    fixed merge error in launcher

--- Hangman/src/ui/Launcher.java
+++ Hangman/src/ui/Launcher.java
@@ -6,25 +6,17 @@ import domain.DomainException;
 import domain.Speler;
 
 public class Launcher {
-<<<<<<< HEAD
 
-	public static void main(String[] args) {
-
-		JFrame f = new JFrame();
-		JOptionPane.showInputDialog(f, "Welkom! \nHoe heet je?");
-		JOptionPane.showMessageDialog(f, "... heeft als score: 1");
-		Object[] shapes = { "Cirkel", "Rechthoek" };
-		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE,
-				null, shapes, null);
-=======
 	
 	public static void main(String[] args) throws DomainException {
 		
 		JFrame f = new JFrame();
 		String speler = JOptionPane.showInputDialog(f, "Welkom! \nHoe heet je?");
 		Speler player = new Speler(speler);
+		Object[] shapes = { "Cirkel", "Rechthoek" };
+		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE,
+				null, shapes, null);
 		JOptionPane.showMessageDialog(f, player.toString()+" heeft als score: "+player.getScore());
 		new PictionaryUi(player);
->>>>>>> e20ca9e90eb14aa6a37fbad46d54d799f33bbf80
 	}
 }
