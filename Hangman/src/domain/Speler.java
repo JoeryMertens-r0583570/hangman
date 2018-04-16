a11f8781a3f348739dec796cb6662f42ea03a05d
Wiietse Vandeput <wietse.vandeput@hotmail.com>
2018-04-16 11:24:29 +0200

    initial project setup added

---
+++ Hangman/src/domain/Speler.java
@@ -0,0 +1,39 @@
+package domain;
+
+public class Speler {
+	
+	private String naam;
+	private int score;
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public Speler(String naam) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addToScore(int score) {
+		
+	}
+	
+	public boolean equals(Object object) {
+		return false;
+	}
+	
+	public String toString() {
+		return "";
+	}
+}
