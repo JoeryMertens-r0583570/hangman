3ff86fe0a84222b6d13fb3512a024975a62bafe0
Unknown <joery.mertens@student.ucll.be>
2018-04-16 14:26:55 +0200

    punt class

--- Hangman/src/domain/Punt.java
+++ Hangman/src/domain/Punt.java
@@ -1,5 +1,36 @@
-package projectje;
+package domain;
 
 public class Punt {
+	private int x;
+	private int y;
 
+	public Punt(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public getX() {
+		return x;
+	}
+
+	public getY() {
+		return y;
+	}
+
+	public setX(int x) {
+		this.x=x;
+	}
+
+	public setY(int y) {
+		this.y=y;
+	}
+
+	public toString() {
+		return "positie: ("+x+", "+y+")";
+	}
+
+	public equals(Punt p) {
+		if(p.getX()==x&&p.getY==y)return true;
+		return false;
+	}
 }
--- Hangman/src/ui/PictonaryUi.java
+++ Hangman/src/ui/PictonaryUi.java
@@ -1,4 +1,4 @@
-package projectje;
+package ui;
 
 public class PictonaryUi {
 
