ab855b215078f6c914596134808d542d3a2559a6
Unknown <joery.mertens@student.ucll.be>
2018-04-16 15:02:41 +0200

    ui works

--- Hangman/src/ui/Launcher.java
+++ Hangman/src/ui/Launcher.java
@@ -2,12 +2,17 @@ package ui;
 
 import javax.swing.*;
 
+import domain.DomainException;
+import domain.Speler;
+
 public class Launcher {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DomainException {
 		
 		JFrame f = new JFrame();
-		JOptionPane.showInputDialog(f, "Welkom! \nHoe heet je?");
-		JOptionPane.showMessageDialog(f, "... heeft als score: 1");
+		String speler = JOptionPane.showInputDialog(f, "Welkom! \nHoe heet je?");
+		Speler player = new Speler(speler);
+		JOptionPane.showMessageDialog(f, player.toString()+" heeft als score: "+player.getScore());
+		new PictionaryUi(player);
 	}
 }
