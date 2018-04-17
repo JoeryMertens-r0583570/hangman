f522b9bf5152bc96fd0b355cf5768d511d9a0f30
stevenzegers <steven.zegers@telenet.be>
2018-04-17 13:36:13 +0200

    AllTests af

--- Hangman/test/test/AllTests.java
+++ Hangman/test/test/AllTests.java
@@ -9,6 +9,13 @@ import org.junit.runners.Suite.SuiteClasses;
 import static org.junit.Assert.*;
 
 public class AllTests {
+	
+	private String naam;
+	private String anderenaam;
+	private int positiveScore;
+	private int negativeScore;
+	private Speler speler;
+	
 	private Punt punt1 = new Punt(10, 20);
 	private Punt zelfdeAlsPunt1 = new Punt(10, 20);
 	private Punt verschillendVanPunt1 = new Punt(15, 20);
@@ -23,22 +30,71 @@ public class AllTests {
 	private Punt lijnstukPunt2 = new Punt(190, 30);
     private Punt lijnstukZelfdeAlsPunt2 = new Punt(190, 30);
 	
+    
+    private Punt linkerBovenhoek;
+	private int breedte, hoogte;
+	private Omhullende omhullende;
+	
+	private int xCoordinaat;
+	private int yCoordinaat;
+	private Punt punt;
+	
+	private Rechthoek rechthoek;
+	
+	private Vorm gebouw;
+    private Vorm dak;
+    private Vorm deur;
+    private Vorm raam;
+    private Vorm deurknop;
+    private Vorm raambalk1;
+    private Vorm raambalk2;
+    private Vorm schouwNietInTekening;
+	
+	@Before
+	public void setUp() {
+		naam = "Lars";
+		anderenaam = "Lies";
+		positiveScore = 5;
+		negativeScore = -5;
+		speler = new Speler(naam);
+		
+		linkerBovenhoek = new Punt(200, 200);
+		breedte = 20;
+		hoogte = 40;
+		omhullende = new Omhullende(linkerBovenhoek,breedte, hoogte);
+		
+		xCoordinaat = 5;
+		yCoordinaat = 10;
+		punt = new Punt(xCoordinaat, yCoordinaat);
+		
+		rechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte);
+		
+		gebouw = new Rechthoek(new Punt(100, 200), 200, 180);
+        dak = new Driehoek(new Punt(100, 200), new Punt(300, 200), new Punt(200, 100));
+        deur = new Rechthoek(new Punt(130, 280), 50,100);
+        raam = new Rechthoek(new Punt(210, 220), 80, 60);
+        deurknop = new Cirkel(new Punt(170, 320), 2);
+        raambalk1 = new LijnStuk(new Punt(210, 250), new Punt(290, 250));
+        raambalk2 = new LijnStuk(new Punt(250, 220), new Punt(250, 280));
+        schouwNietInTekening = new Rechthoek(new Punt(150, 150), 20,40);
+	}
+	
 	//CirkelTest
 	@Test
     public void testCirkelAanmakenCorrect() {
         Punt punt = new Punt(200, 200);
         Cirkel cirkel = new Cirkel(punt, 20);
     }
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = DomainException.class)
     public void testCirkelAanmakenMiddelpuntNull() {
         Cirkel cirkel = new Cirkel(null, 20);
     }
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = DomainException.class)
     public void testCirkelAanmakenStraalKleinerDan0() {
         Punt punt = new Punt(200, 200);
         Cirkel cirkel = new Cirkel(punt, -20);
     }
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = DomainException.class)
     public void testCirkelAanmakenStraalGelijkAan0() {
         Punt punt = new Punt(200, 200);
         Cirkel cirkel = new Cirkel(punt, 0);
@@ -165,6 +221,339 @@ public class AllTests {
     }
     
     //OmhullendeTest
-    
+    @Test
+	public void Omhullende_moet_omhullende_aanmaken_met_gegeven_linkerbovenhoek_breedte_en_hoogte() {
+		Omhullende omhullende = new Omhullende(linkerBovenhoek,breedte, hoogte);
+		assertEquals(linkerBovenhoek, omhullende.getLinkerBovenhoek());
+		assertEquals(hoogte, omhullende.getHoogte());
+		assertEquals(breedte, omhullende.getBreedte());
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Omhullende_Moet_exception_gooien_als_linkerbovenhoek_null()  {
+		new Omhullende(null, breedte, hoogte);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Omhullende_Moet_exception_gooien_als_breedte_kleiner_dan_0()  {
+		new Omhullende(linkerBovenhoek, -1, hoogte);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Omhullende_Moet_exception_gooien_als_hoogte_kleiner_dan_0()  {
+		new Omhullende(linkerBovenhoek, breedte, -1);
+	}
 
+	@Test
+	public void Omhullende_Moet_omhullende_maken_als_breedte_gelijk_aan_0()  {
+		Omhullende omhullende = new Omhullende(linkerBovenhoek,0, hoogte);
+		assertEquals(0, omhullende.getBreedte());
+	}
+	
+	@Test
+	public void Omhullende_Moet_omhullende_maken_als_hoogte_gelijk_aan_0()  {
+		Omhullende omhullende = new Omhullende(linkerBovenhoek,breedte, 0);
+		assertEquals(0, omhullende.getHoogte());
+	}
+	
+	@Test
+	public void Omhullend_equals_moet_true_teruggeven_als_linkerbovenhoek_breedte_hoogte_gelijk_zijn(){
+		Omhullende zelfdeOmhullende = new Omhullende(linkerBovenhoek,breedte, hoogte);
+		assertTrue(omhullende.equals(zelfdeOmhullende));
+	}
+	
+	@Test
+	public void Omhullend_equals_moet_false_teruggeven_als_linkerbovenhoek_verschilt(){
+		Punt andereLinkerBovenhoek = new Punt(100,100);
+		Omhullende verschillendeOmhullende = new Omhullende(andereLinkerBovenhoek, breedte, hoogte);
+		assertFalse(omhullende.equals(verschillendeOmhullende));
+	}
+	
+	@Test
+	public void Omhullend_equals_moet_false_teruggeven_als_breedte_verschilt(){
+		Omhullende verschillendeOmhullende = new Omhullende(linkerBovenhoek, breedte + 10, hoogte);
+		assertFalse(omhullende.equals(verschillendeOmhullende));
+	}
+	
+	@Test
+	public void Omhullend_equals_moet_false_teruggeven_als_hoogte_verschilt(){
+		Omhullende verschillendeOmhullende = new Omhullende(linkerBovenhoek, breedte, hoogte + 10);
+		assertFalse(omhullende.equals(verschillendeOmhullende));
+	}
+	
+	@Test
+	public void Omhullend_equals_moet_false_teruggeven_als_parameter_null(){
+		assertFalse(omhullende.equals(null));
+	}
+	
+	//PuntTest
+	
+	@Test
+	public void Punt_moet_een_punt_maken_met_de_gegeven_coordinaten() {
+		punt = new Punt(xCoordinaat, yCoordinaat);
+		assertEquals(xCoordinaat, punt.getX());
+		assertEquals(yCoordinaat, punt.getY());
+	}
+	
+	public void punt_equals_moet_true_teruggeven_als_x_en_y_coordinaat_gelijk_zijn(){
+		Punt puntAnder = new Punt(xCoordinaat, yCoordinaat);
+		assertTrue(punt.equals(puntAnder));
+	}
+	
+	@Test
+	public void punt_equals_moet_false_teruggeven_als_parameter_null(){
+		assertFalse(punt.equals(null));
+	}
+	
+	@Test
+	public void punt_equals_moet_false_teruggeven_als_punt_verschillende_x_coordinaat_heeft(){
+		Punt puntAnder = new Punt(xCoordinaat-1, yCoordinaat);
+		assertFalse(punt.equals(puntAnder));
+	}
+	
+	@Test
+	public void punt_equals_moet_false_teruggeven_als_punt_verschillende_y_coordinaat_heeft(){
+		Punt puntAnder = new Punt(xCoordinaat, yCoordinaat-1);
+		assertFalse(punt.equals(puntAnder));
+	}
+	
+	//RechthoekTest
+	@Test
+	public void Rechthoek_moet_rechthoek_aanmaken_met_gegeven_linkerbovenhoek_breedte_en_hoogte() throws DomainException {
+		Rechthoek rechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte);
+		assertEquals(linkerBovenhoek, rechthoek.getLinkerBovenhoek());
+		assertEquals(hoogte, rechthoek.getHoogte());
+		assertEquals(breedte, rechthoek.getBreedte());
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Rechthoek_Moet_exception_gooien_als_linkerbovenhoek_null() throws DomainException  {
+		new Rechthoek(null, breedte, hoogte);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Rechthoek_Moet_exception_gooien_als_breedte_kleiner_dan_0() throws DomainException  {
+		new Rechthoek(linkerBovenhoek, -1, hoogte);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Rechthoek_Moet_exception_gooien_als_hoogte_kleiner_dan_0() throws DomainException  {
+		new Rechthoek(linkerBovenhoek, breedte, -1);
+		
+	}
+
+	@Test (expected = DomainException.class)
+	public void Rechthoek_Moet_exception_gooien_als_breedte_gelijk_aan_0() throws DomainException  {
+		new Rechthoek(linkerBovenhoek, 0, hoogte);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Rechthoek_Moet_exception_gooien_als_hoogte_gelijk_aan_dan_0() throws DomainException  {
+		new Rechthoek(linkerBovenhoek, breedte, 0);
+	}
+	
+	@Test
+	public void Rechthoek_equals_moet_true_teruggeven_als_linkerbovenhoek_breedte_hoogte_gelijk_zijn() throws DomainException{
+		Rechthoek zelfdeRechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte);
+		assertTrue(rechthoek.equals(zelfdeRechthoek));
+	}
+	
+	@Test
+	public void Rechthoek_equals_moet_false_teruggeven_als_linkerbovenhoek_verschilt() throws DomainException{
+		Punt andereLinkerBovenhoek = new Punt(100,100);
+		Rechthoek verschillendeRechthoek = new Rechthoek(andereLinkerBovenhoek, breedte, hoogte);
+		assertFalse(rechthoek.equals(verschillendeRechthoek));
+	}
+	
+	@Test
+	public void Rechthoek_equals_moet_false_teruggeven_als_breedte_verschilt() throws DomainException{
+		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte + 10, hoogte);
+		assertFalse(rechthoek.equals(verschillendeRechthoek));
+	}
+	
+	@Test
+	public void Rechthoek_equals_moet_false_teruggeven_als_hoogte_verschilt() throws DomainException{
+		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte, hoogte + 10);
+		assertFalse(rechthoek.equals(verschillendeRechthoek));
+	}
+	
+	@Test
+	public void Rechthoek_equals_moet_false_teruggeven_als_parameter_null(){
+		assertFalse(rechthoek.equals(null));
+	}
+	
+	//SpelerTest
+	@Test
+	public void Speler_moet_speler_aanmaken_met_gegeven_naam() throws DomainException {
+		speler = new Speler(naam);
+		assertEquals(naam, speler.getNaam());
+		assertEquals(0, speler.getScore());
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Speler_moet_exception_gooien_als_naam_null() throws DomainException {
+		speler = new Speler(null);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void Speler_moet_exception_gooien_als_naam_lege_string() throws DomainException {
+		speler = new Speler("");
+	}
+	
+	@Test
+	public void equals_moet_true_teruggeven_als_naam_en_score_gelijk_zijn() throws DomainException{
+		speler.addToScore(positiveScore);
+		Speler andereSpeler = new Speler(new String("Lars"));
+		andereSpeler.addToScore(positiveScore);
+		
+		assertTrue(speler.equals(andereSpeler));
+	}
+	
+	@Test
+	public void Speler_equals_moet_false_teruggeven_als_parameter_null(){
+		assertFalse(speler.equals(null));
+	}
+	
+	@Test
+	public void equals_moet_false_teruggeven_als_speler_een_andere_naam_heeft() throws DomainException{
+		Speler andereSpeler = new Speler(anderenaam);
+		assertFalse(speler.equals(andereSpeler));
+	}
+	
+	@Test
+	public void equals_moet_false_teruggeven_als_speler_aan_andere_score_heeft() throws DomainException{
+		Speler andereSpeler = new Speler(naam);
+		andereSpeler.addToScore(positiveScore);
+		assertFalse(speler.equals(andereSpeler));
+	}
+	
+	@Test
+	public void addToScore_moet_gegeven_score_toevoegen_aan_bestaande_score() throws DomainException{
+		speler.addToScore(positiveScore);
+		assertEquals(positiveScore, speler.getScore());
+	}
+	
+	@Test
+	public void addtoScore_mag_negatieve_score_toevoegen_als_de_resulterende_score_niet_negatief_wordt() throws DomainException{
+		speler.addToScore(positiveScore);
+		speler.addToScore(positiveScore);
+		speler.addToScore(negativeScore);
+		assertEquals(positiveScore, speler.getScore());
+	}
+	
+	@Test (expected = DomainException.class)
+	public void addToScore_moet_exception_gooien_als_resulterende_score_negatief_wordt() throws DomainException{
+		speler.addToScore(negativeScore);
+	}
+	
+	//TekeningTest
+	@Test
+    public void Tekening_moet_een_tekening_aanmaken_met_een_geldige_naam_en_0_vormen() {
+        Tekening huis = new Tekening("huis");
+        assertEquals("huis", huis.getNaam());
+        assertEquals(0, huis.getAantalVormen());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void Tekening_moet_exception_gooien_als_naam_null() {
+        new Tekening(null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void Tekening_moet_exception_gooien_als_naam_leeg() {
+        new Tekening("");
+    }
+
+    @Test
+    public void getAantalVormen_moet_aantal_vormen_teruggeven() {
+        Tekening huis = createHuisZonderShouw();
+        assertEquals(7, huis.getAantalVormen());
+    }
+
+    @Test
+    public void bevat_geeft_true_als_gegeven_vorm_deel_uitmaakt_van_de_tekening(){
+        Tekening huis = createHuisZonderShouw();
+        assertTrue(huis.bevat(deur));
+    }
+
+    @Test
+    public void bevat_geeft_false__als_gegeven_vorm_geen_deel_uitmaakt_van_de_tekening(){
+        Tekening huis = createHuisZonderShouw();
+        assertFalse(huis.bevat(schouwNietInTekening));
+    }
+
+    @Test
+    public void Tekening_equals_moet_false_teruggeven_als_parameter_null(){
+        Tekening huis = createHuisZonderShouw();
+        assertFalse(huis.equals(null));
+    }
+
+    @Test
+    public void equals_moet_false_teruggeven_als_parameter_tekening_is_met_verschillend_aantal_vormen(){
+        Tekening huis = createHuisZonderShouw();
+        Tekening huisMetSchouw = createHuisMetSchouw();
+        assertFalse(huis.equals(huisMetSchouw));
+    }
+
+    @Test
+    public void equals_moet_false_teruggeven_als_parameter_tekening_is_met_zelfde_aantal_vormen_ander_vorm(){
+        Tekening huis = createHuisZonderShouw();
+        Tekening huisMetSchouwZonderDeur = createHuisMetSchouwZonderDeur();
+        assertFalse(huis.equals(huisMetSchouwZonderDeur));
+    }
+
+    @Test
+    public void equals_moet_true_teruggeven_als_parameter_tekening_is_met_zelfde_aantal_vormen_andere_volgorde(){
+        Tekening huis = createHuisZonderShouw();
+        Tekening huisMetSchouwZonderDeur = createHuisMetSchouwZonderDeur();
+        huisMetSchouwZonderDeur.verwijder(schouwNietInTekening);
+        huisMetSchouwZonderDeur.voegToe(deur);
+        assertTrue(huis.equals(huisMetSchouwZonderDeur));
+    }
+
+    @Test
+    public void equals_moet_true_teruggeven_alsparameter_tekening_is_met_zelfde_aantal_vormen_zelfde_volgorde(){
+        Tekening huis = createHuisZonderShouw();
+        Tekening huisMetSchouw = createHuisMetSchouw();
+        huisMetSchouw.verwijder(schouwNietInTekening);
+        assertTrue(huis.equals(huisMetSchouw));
+    }
+
+
+    public Tekening createHuisMetSchouw() {
+        Tekening huisMetSchouw = new Tekening("huisMetSchouw");
+        huisMetSchouw.voegToe(gebouw);
+        huisMetSchouw.voegToe(dak);
+        huisMetSchouw.voegToe(deur);
+        huisMetSchouw.voegToe(raam);
+        huisMetSchouw.voegToe(deurknop);
+        huisMetSchouw.voegToe(raambalk1);
+        huisMetSchouw.voegToe(raambalk2);
+        huisMetSchouw.voegToe(schouwNietInTekening);
+        return huisMetSchouw;
+    }
+
+    public Tekening createHuisZonderShouw() {
+        Tekening huis = new Tekening("huis");
+        huis.voegToe(gebouw);
+        huis.voegToe(dak);
+        huis.voegToe(deur);
+        huis.voegToe(raam);
+        huis.voegToe(deurknop);
+        huis.voegToe(raambalk1);
+        huis.voegToe(raambalk2);
+        return huis;
+    }
+
+    public Tekening createHuisMetSchouwZonderDeur() {
+        Tekening huisMetSchouwZonderDeur = new Tekening("huisMetSchouwZonderDeur");
+        huisMetSchouwZonderDeur.voegToe(gebouw);
+        huisMetSchouwZonderDeur.voegToe(dak);
+        huisMetSchouwZonderDeur.voegToe(raam);
+        huisMetSchouwZonderDeur.voegToe(deurknop);
+        huisMetSchouwZonderDeur.voegToe(raambalk1);
+        huisMetSchouwZonderDeur.voegToe(raambalk2);
+        huisMetSchouwZonderDeur.voegToe(schouwNietInTekening);
+        return huisMetSchouwZonderDeur;
+    }
 }
