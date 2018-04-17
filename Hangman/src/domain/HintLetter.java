67c007b4947c5d75e52fec87db9b30d8c20d5ba3
Wiietse Vandeput <wietse.vandeput@hotmail.com>
2018-04-17 12:11:52 +0200

    hintLetter en hintWoord werken beiden en de tests zijn allemaal groen

---
+++ Hangman/src/domain/HintLetter.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class HintLetter {
+
+//	temp
+	private char letter;
+	private boolean isgeraden = false;
+	
+	public HintLetter(char letter) {
+		this.letter = letter;
+	}
+	
+	public boolean raad(char letter) {
+		if (isgeraden) return false;
+		if (Character.toLowerCase(this.letter) == Character.toLowerCase(letter)) {
+			isgeraden = true;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isGeraden() {
+		return isgeraden;
+	}
+	
+	public char toChar() {
+		if (isgeraden) {
+			return this.letter;
+		}
+		return '_';
+	}
+	
+	public char getLetter() {
+		return this.letter;
+	}
+}
---
+++ Hangman/src/domain/HintWoord.java
@@ -0,0 +1,50 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HintWoord {
+	
+	private List<HintLetter> letterList = new ArrayList<HintLetter>(); 
+	private boolean isgeraden;
+
+	public HintWoord(String woord) {
+		if (woord == null ||  woord.isEmpty()) throw new DomainException("woord mag niet NULL zijn");
+		for (int i = 0; i < woord.length();i++) {
+			letterList.add(new HintLetter(woord.charAt(i)));
+		}
+		
+	}
+	
+	public boolean raad(char letter) {
+		boolean juist = false;
+		for(HintLetter hintLetter: letterList) {
+			if(hintLetter.raad(letter)) {
+				juist = true;
+			}
+			
+		}
+		return juist;
+	}
+	
+	public boolean isGeraden() {
+		if (this.toString().replace(" ", "").equals(this.getWoord())) return true;
+		return false;
+	}
+	
+	public String toString() {
+		String hint = "";
+		for(HintLetter hintLetter: letterList) {
+			hint += (" "+ hintLetter.toChar());
+		}
+		return hint.trim();
+	}
+	
+	public String getWoord() {
+		String woord = "";
+		for(HintLetter hintLetter: letterList) {
+			woord += (" "+ hintLetter.getLetter());
+		}
+		return woord.trim().replace(" ", "");
+	}
+}
